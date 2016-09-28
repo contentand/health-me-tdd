d4389abad1a8cd69a1362cc0d1bf9dc8977facb1
Daniil Yurov <Daniil_Yurov@epam.com>
2016-09-28 12:05:39 +0300

    Initial Commit

---
+++ src/main/java/com/dy/health/HealthService.java
@@ -0,0 +1,5 @@
+package com.dy.health;
+
+public class HealthService {
+
+}
---
+++ src/test/java/com/dy/health/HealthServiceTest.java
@@ -0,0 +1,16 @@
+package com.dy.health;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.sql.Date;
+import java.time.LocalDateTime;
+
+import static org.junit.Assert.*;
+
+public class HealthServiceTest {
+
+    private HealthService healthService;
+
+
+}
