e511695306652ab63650ac17cd11aee5b1385f39
Daniil Yurov <Daniil_Yurov@epam.com>
2016-09-28 12:11:46 +0300

    added test case canDrinkWaterInGlasses()

--- src/main/java/com/dy/health/HealthService.java
+++ src/main/java/com/dy/health/HealthService.java
@@ -1,5 +1,14 @@
 package com.dy.health;
 
+import java.time.LocalDateTime;
+
 public class HealthService {
 
+    public void drink(String drinkName, String container, double quantity, LocalDateTime dateTime) {
+
+    }
+
+    public double drunk(String glass, LocalDateTime requestDateTime) {
+        return 0;
+    }
 }
--- src/test/java/com/dy/health/HealthServiceTest.java
+++ src/test/java/com/dy/health/HealthServiceTest.java
@@ -3,14 +3,34 @@ package com.dy.health;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.sql.Date;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
 public class HealthServiceTest {
 
     private HealthService healthService;
 
+    @Before
+    public void setUp() throws Exception {
+        this.healthService = new HealthService();
+    }
 
+    @Test
+    public void canDrinkWaterInGlasses() throws Exception {
+        // given
+        String beverage = "water";
+        String container = "glass";
+        double quantityDrunk = 1;
+        String dateTime = "2016-09-28T08:00:00";
+        String currentDateTime = "2016-09-28T23:59:59";
+        // when
+        healthService.drink(beverage, container, quantityDrunk, LocalDateTime.parse(dateTime));
+        // then
+        double precision = 0.00001;
+        double actualQuantityDrunk = healthService.drunk(container, LocalDateTime.parse(currentDateTime));
+        assertEquals(quantityDrunk, actualQuantityDrunk, precision);
+    }
 }
