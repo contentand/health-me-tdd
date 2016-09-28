04688e8367b1d69c204b5485cb6469a8d2450124
Daniil Yurov <Daniil_Yurov@epam.com>
2016-09-28 13:33:25 +0300

    satisfied test canHaveBreakfast()

--- src/main/java/com/dy/health/HealthService.java
+++ src/main/java/com/dy/health/HealthService.java
@@ -2,8 +2,10 @@ package com.dy.health;
 
 import com.sun.org.apache.regexp.internal.RE;
 
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -13,6 +15,8 @@ import java.util.stream.Stream;
 public class HealthService {
 
     private Map<LocalDate, List<Record>> records = new HashMap<>();
+
+
 
     private static class Record {
         String type; // drink
@@ -36,6 +40,23 @@ public class HealthService {
             this.quantity = record.quantity;
             this.dateTime = record.dateTime;
         }
+
+        @Override
+        public String toString() {
+            return "Record{" +
+                    "type='" + type + '\'' +
+                    ", name='" + name + '\'' +
+                    ", container='" + container + '\'' +
+                    ", quantity=" + quantity +
+                    ", dateTime=" + dateTime +
+                    '}';
+        }
+    }
+
+    public void eat(String foodName, String container, double quantity, LocalDateTime dateTime) {
+        LocalDate date = dateTime.toLocalDate();
+        records.putIfAbsent(date, new ArrayList<>());
+        records.get(date).add(new Record("food", foodName, container, quantity, dateTime));
     }
 
     public void drink(String drinkName, String container, double quantity, LocalDateTime dateTime) {
@@ -46,17 +67,27 @@ public class HealthService {
 
     public double drunk(String container, LocalDateTime requestDateTime) {
         LocalDate date = requestDateTime.toLocalDate();
-        return calculate("drink", container, date);
+        return calculate("drink", container, "all", date);
     }
 
-    private double calculate(String type, String container, LocalDate date) {
+    public double eaten(String meal, String container, LocalDateTime requestDateTime) {
+        LocalDate date = requestDateTime.toLocalDate();
+        return calculate("food", container, meal, date);
+    }
+
+    private double calculate(String type, String container, String timeRange, LocalDate date) {
         Stream<Record> recordsForDate = records.get(date).stream();
         return recordsForDate
                 .filter(record -> record.type.equals(type))
                 .map(record -> {
                     if (record.container.equals(container)) {
-                        return record.quantity;
+                        if (isWithinTimeRange(timeRange, record.dateTime.toLocalTime())) {
+                            return record.quantity;
+                        } else {
+                            return 0D;
+                        }
                     } else {
+                        System.out.println(container + " " + record.toString());
                         throw new IllegalStateException(); // to be improved later
                     }
                 })
@@ -64,6 +95,16 @@ public class HealthService {
                 .orElse(0D);
     }
 
+    private boolean isWithinTimeRange(String timeRange, LocalTime localTime) {
+        if (timeRange.equals("all")) {
+            return true;
+        } else if (timeRange.equals("breakfast")) {
+            LocalTime breakfastStart = LocalTime.parse("02:00");
+            LocalTime breakfastEnd = LocalTime.parse("12:00");
+            return localTime.isAfter(breakfastStart) && localTime.isBefore(breakfastEnd);
+        }
+        return false;
+    }
 
 
 }
--- src/test/java/com/dy/health/HealthServiceTest.java
+++ src/test/java/com/dy/health/HealthServiceTest.java
@@ -33,4 +33,27 @@ public class HealthServiceTest {
         double actualQuantityDrunk = healthService.drunk(container, LocalDateTime.parse(currentDateTime));
         assertEquals(quantityDrunk, actualQuantityDrunk, precision);
     }
+
+    @Test
+    public void canHaveBreakfast() throws Exception {
+        //given
+        String food = null;
+        String container = "kilocal";
+        double quantityEaten = 200;
+        String breakfastDateTime = "2016-09-28T08:30:00";
+        String lunchDateTime = "2016-09-28T13:30:00";
+        String dinnerDateTime = "2016-09-28T19:30:00";
+        String supperDateTime = "2016-09-28T22:30:00";
+        String currentDateTime = "2016-09-28T23:59:59";
+        //when
+        healthService.eat(food, container, quantityEaten, LocalDateTime.parse(breakfastDateTime));
+        healthService.eat(food, container, quantityEaten, LocalDateTime.parse(lunchDateTime));
+        healthService.eat(food, container, quantityEaten, LocalDateTime.parse(dinnerDateTime));
+        healthService.eat(food, container, quantityEaten, LocalDateTime.parse(supperDateTime));
+        //then
+        String meal = "breakfast";
+        double precision = 0.00001;
+        double actualBreakfastEaten = healthService.eaten(meal, container, LocalDateTime.parse(currentDateTime));
+        assertEquals(quantityEaten, actualBreakfastEaten, precision);
+    }
 }
