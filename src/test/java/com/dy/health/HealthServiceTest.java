9a9fd337f257e7d5e63d8cc70733f078a318b22d
Daniil Yurov <Daniil_Yurov@epam.com>
2016-09-28 19:26:08 +0300

    refactored test code

--- src/test/java/com/dy/health/HealthServiceTest.java
+++ src/test/java/com/dy/health/HealthServiceTest.java
@@ -11,6 +11,8 @@ import static org.junit.Assert.*;
 public class HealthServiceTest {
 
     private HealthService healthService;
+    private LocalDate currentDate = LocalDate.parse("2016-09-28");
+    private double precision = 0.00001;
 
     @Before
     public void setUp() throws Exception {
@@ -20,81 +22,60 @@ public class HealthServiceTest {
     @Test
     public void canDrinkWaterInGlasses() throws Exception {
         // given
-        String beverage = "water";
-        String container = "glass";
         double quantityDrunk = 1;
-        String dateTime = "2016-09-28T08:00:00";
-        String currentDate = "2016-09-28";
         // when
-        healthService.drink(beverage, container, quantityDrunk, LocalDateTime.parse(dateTime));
+        healthService.drink("water", "glass", quantityDrunk, LocalDateTime.parse("2016-09-28T08:00:00"));
         // then
-        double precision = 0.00001;
-        double actualQuantityDrunk = healthService.drunk(container, LocalDate.parse(currentDate));
+        double actualQuantityDrunk = healthService.drunk("glass", currentDate);
         assertEquals(quantityDrunk, actualQuantityDrunk, precision);
     }
 
     @Test
     public void canHaveBreakfast() throws Exception {
         //given
-        String food = null;
-        String container = "kilocal";
         double quantityEaten = 200;
         String breakfastDateTime = "2016-09-28T08:30:00";
         String lunchDateTime = "2016-09-28T13:30:00";
         String dinnerDateTime = "2016-09-28T19:30:00";
         String supperDateTime = "2016-09-28T22:30:00";
-        String currentDate = "2016-09-28";
         //when
-        healthService.eat(food, container, quantityEaten, LocalDateTime.parse(breakfastDateTime));
-        healthService.eat(food, container, quantityEaten, LocalDateTime.parse(lunchDateTime));
-        healthService.eat(food, container, quantityEaten, LocalDateTime.parse(dinnerDateTime));
-        healthService.eat(food, container, quantityEaten, LocalDateTime.parse(supperDateTime));
+        healthService.eat(null, "kilocal", quantityEaten, LocalDateTime.parse(breakfastDateTime));
+        healthService.eat(null, "kilocal", quantityEaten, LocalDateTime.parse(lunchDateTime));
+        healthService.eat(null, "kilocal", quantityEaten, LocalDateTime.parse(dinnerDateTime));
+        healthService.eat(null, "kilocal", quantityEaten, LocalDateTime.parse(supperDateTime));
         //then
-        String meal = "breakfast";
-        double precision = 0.00001;
-        double actualBreakfastEaten = healthService.eaten(meal, container, LocalDate.parse(currentDate));
+        double actualBreakfastEaten = healthService.eaten("breakfast", "kilocal", currentDate);
         assertEquals(quantityEaten, actualBreakfastEaten, precision);
     }
 
     @Test
     public void canDrinkSeveralGlassesOfWaterAtDifferentTimes() throws Exception {
         // given
-        String beverage = "water";
-        String container = "glass";
         double quantityDrunk = 1;
-        String dateTime1 = "2016-09-28T08:00:00";
-        String dateTime2 = "2016-09-28T10:00:00";
-        String currentDate = "2016-09-28";
         // when
-        healthService.drink(beverage, container, quantityDrunk, LocalDateTime.parse(dateTime1));
-        healthService.drink(beverage, container, quantityDrunk, LocalDateTime.parse(dateTime2));
+        healthService.drink("water", "glass", quantityDrunk, LocalDateTime.parse("2016-09-28T08:00:00"));
+        healthService.drink("water", "glass", quantityDrunk, LocalDateTime.parse("2016-09-28T10:00:00"));
         // then
-        double precision = 0.00001;
-        double actualQuantityDrunk = healthService.drunk(container, LocalDate.parse(currentDate));
+        double actualQuantityDrunk = healthService.drunk("glass", currentDate);
         assertEquals(quantityDrunk * 2, actualQuantityDrunk, precision);
     }
 
     @Test
     public void canHaveLunch() throws Exception {
         //given
-        String food = null;
-        String container = "kilocal";
         double quantityEaten = 200;
         double lunchQuantityEaten = 1403;
         String breakfastDateTime = "2016-09-28T08:30:00";
         String lunchDateTime = "2016-09-28T13:30:00";
         String dinnerDateTime = "2016-09-28T19:30:00";
         String supperDateTime = "2016-09-28T22:30:00";
-        String currentDate = "2016-09-28";
         //when
-        healthService.eat(food, container, quantityEaten, LocalDateTime.parse(breakfastDateTime));
-        healthService.eat(food, container, lunchQuantityEaten, LocalDateTime.parse(lunchDateTime));
-        healthService.eat(food, container, quantityEaten, LocalDateTime.parse(dinnerDateTime));
-        healthService.eat(food, container, quantityEaten, LocalDateTime.parse(supperDateTime));
+        healthService.eat(null, "kilocal", quantityEaten, LocalDateTime.parse(breakfastDateTime));
+        healthService.eat(null, "kilocal", lunchQuantityEaten, LocalDateTime.parse(lunchDateTime));
+        healthService.eat(null, "kilocal", quantityEaten, LocalDateTime.parse(dinnerDateTime));
+        healthService.eat(null, "kilocal", quantityEaten, LocalDateTime.parse(supperDateTime));
         //then
-        String meal = "lunch";
-        double precision = 0.00001;
-        double actualBreakfastEaten = healthService.eaten(meal, container, LocalDate.parse(currentDate));
+        double actualBreakfastEaten = healthService.eaten("lunch", "kilocal", currentDate);
         assertEquals(lunchQuantityEaten, actualBreakfastEaten, precision);
     }
 
@@ -103,21 +84,17 @@ public class HealthServiceTest {
         //given
         String moveStart = "2016-09-28T09:30:00";
         String moveEnd = "2016-09-28T11:30:00";
-        String container = "step";
         double quantity = 1234;
-        String currentDate = "2016-09-28";
         //when
-        healthService.move(container, quantity, LocalDateTime.parse(moveStart),LocalDateTime.parse(moveEnd));
+        healthService.move("step", quantity, LocalDateTime.parse(moveStart), LocalDateTime.parse(moveEnd));
         //then
-        double precision = 0.00001;
-        double actualMoved = healthService.moved(container, LocalDate.parse(currentDate));
+        double actualMoved = healthService.moved("step", currentDate);
         assertEquals(quantity, actualMoved, precision);
     }
 
     @Test
     public void canReportHowMuchIsLeftForTheDay() throws Exception {
         // given
-        String currentDate = "2016-09-28";
         // when
         healthService.drink("water", "glass", 1, LocalDateTime.parse("2016-09-28T08:14:00"));
         healthService.eat("sandwich", "kilocal", 204, LocalDateTime.parse("2016-09-28T08:30:00"));
@@ -136,8 +113,7 @@ public class HealthServiceTest {
         healthService.drink("water", "glass", 3, LocalDateTime.parse("2016-09-28T14:44:00"));
         healthService.eat("pelmeni", "kilocal", 704, LocalDateTime.parse("2016-09-28T19:30:00"));
         // then
-        double precision = 0.00001;
-        ReportLeft reportLeft = healthService.reportLeft(LocalDate.parse(currentDate));
+        ReportLeft reportLeft = healthService.reportLeft(currentDate);
         assertEquals(200, reportLeft.getStepsLeft(), precision); // 2000
         assertEquals(0.5, reportLeft.getHoursToMoveLeft(), precision); // 2
         assertEquals(0, reportLeft.getKiloCalsLeft(), precision); //1300
@@ -147,7 +123,6 @@ public class HealthServiceTest {
     @Test
     public void canReportDayStatistics() throws Exception {
         // given
-        String currentDate = "2016-09-28";
         // when
         healthService.drink("water", "glass", 1, LocalDateTime.parse("2016-09-28T08:14:00"));
         healthService.eat("sandwich", "kilocal", 204, LocalDateTime.parse("2016-09-28T08:30:00"));
@@ -166,8 +141,7 @@ public class HealthServiceTest {
         healthService.drink("water", "glass", 3, LocalDateTime.parse("2016-09-28T14:44:00"));
         healthService.eat("pelmeni", "kilocal", 704, LocalDateTime.parse("2016-09-28T19:30:00"));
         // then
-        double precision = 0.00001;
-        DayReport dayReport = healthService.getDayReport(LocalDate.parse(currentDate));
+        DayReport dayReport = healthService.getDayReport(currentDate);
         assertEquals(0.9, dayReport.getStepsCompletionRate(), precision);
         assertEquals(0.75, dayReport.getHoursToMoveCompletionRate(), precision);
         assertEquals(1.0, dayReport.getKiloCalsCompletionRate(), precision);
@@ -178,7 +152,6 @@ public class HealthServiceTest {
     public void canReportStatisticsForFourDayPeriod() throws Exception {
         // given
         String firstDate = "2016-09-25";
-        String currentDate = "2016-09-28";
         // when
         // day 1 : 3 days ago
         healthService.drink("water", "glass", 1, LocalDateTime.parse("2016-09-26T08:14:00"));
@@ -198,6 +171,7 @@ public class HealthServiceTest {
         healthService.drink("water", "glass", 3, LocalDateTime.parse("2016-09-26T14:44:00"));
         healthService.eat("pelmeni", "kilocal", 704, LocalDateTime.parse("2016-09-26T19:30:00"));
         // day 2 : 2 days ago
+            // nothing happened
         // day 3 : 1 day ago
         healthService.drink("water", "glass", 1, LocalDateTime.parse("2016-09-27T08:14:00"));
         healthService.eat("sandwich", "kilocal", 204, LocalDateTime.parse("2016-09-27T08:30:00"));
@@ -229,13 +203,11 @@ public class HealthServiceTest {
         healthService.drink("water", "glass", 3, LocalDateTime.parse("2016-09-28T14:44:00"));
         healthService.eat("pelmeni", "kilocal", 704, LocalDateTime.parse("2016-09-28T19:30:00"));
         // then
-        double precision = 0.00001;
         PeriodReport periodReport = healthService.getPeriodReport(LocalDate.parse(firstDate),
-                LocalDate.parse(currentDate));
+                currentDate);
         assertEquals(1650, periodReport.getStepsCompletionMedian(), precision);
         assertEquals(1.25, periodReport.getHoursToMoveCompletionMedian(), precision);
         assertEquals(1412, periodReport.getKiloCalsCompletionMedian(), precision);
         assertEquals(1.75, periodReport.getLiquidLitersCompletionMedian(), precision);
-
     }
 }
